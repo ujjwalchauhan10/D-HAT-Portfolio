e97d67a08cd2b69b727d89fc044d870779c12d0c
ujjwalchauhan10 <ujjwalchauhan247@gmail.com>
2022-11-27 21:32:57 +0530

    Team

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import vardhan from "../Assets/Team/Harsh.svg";
+import azhar from "../Assets/Team/Azhar.svg";
+import divyansh from "../Assets/Team/Divyansh.svg";
+import tushar from "../Assets/Team/Tushar.svg";
+import ujjwal from "../Assets/Team/Ujjwal.svg";
+import kartikey from "../Assets/Team/Kartikey.svg";
+import naruto from "../Assets/Team/Naruto.svg";
+import aarush from "../Assets/Team/Aarush.svg";
+import aakarsh from "../Assets/Team/Aakarsh.svg";
 
 const Team = () => {
     return (
@@ -15,8 +24,250 @@ const Team = () => {
                         <span className="text-pro px-1">problem solvers</span>
                     </div>
                 </div>
-                <div className="row center">
-                    <div className="col-10"></div>
+                <div className="row center client-spacer">
+                    <div className="col-10">
+                        <div className="marquee">
+                            <div className="marquee__group">
+                                <a
+                                    href="https://www.linkedin.com/in/harsh-vardhan-singh-rawat-7a8679196/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={vardhan}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/tusharagrawal123/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={tushar}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/divyansh-garg20/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={divyansh}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/aakarshverma/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={aakarsh}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/sayedazharsabri/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={azhar}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/ujjwalchauhan"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={ujjwal}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/kartikey-kiroula-56244b201/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={kartikey}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/aarushkashyap/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={aarush}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/devrata-puri-a17b7a1b6/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={naruto}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div aria-hidden="true" className="marquee__group">
+                                <a
+                                    href="https://www.linkedin.com/in/harsh-vardhan-singh-rawat-7a8679196/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={vardhan}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/tusharagrawal123/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={tushar}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/divyansh-garg20/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={divyansh}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/aakarshverma/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={aakarsh}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/sayedazharsabri/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={azhar}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/ujjwalchauhan"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={ujjwal}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/kartikey-kiroula-56244b201/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={kartikey}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/aarushkashyap/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={aarush}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.linkedin.com/in/devrata-puri-a17b7a1b6/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={naruto}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
