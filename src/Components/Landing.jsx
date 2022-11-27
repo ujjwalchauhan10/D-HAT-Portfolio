417ce7c849624043c273c64532b5dd405f2a53b3
ujjwalchauhan10 <ujjwalchauhan247@gmail.com>
2022-11-27 18:11:15 +0530

    Creating Component

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import WeProvide from "./WeProvide";
 const Landing = () => {
     return (
         <div>
@@ -81,63 +81,7 @@ const Landing = () => {
                     </div>
                 </div>
             </div>
-            <div className="container-fluid bg-2">
-                <div className="row">
-                    <div className="provide-line1 mt-5">
-                        “The most suitable
-                        <span className="text-pro px-1">
-                            Blockchain Consultancy
-                        </span>
-                        <br></br>and
-                        <span className="text-pro px-1">WEB 3 Solutions</span>
-                        for your needs.”
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="we-provide">We Provide</div>
-                    <div className="provide-line1">
-                        The
-                        <span className="text-pro px-1">safest</span>
-                        and
-                        <span className="text-pro px-1">easiest-to-use</span>
-                        <br></br>
-                        place for
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="center">
-                        <div className="services">
-                            <div className="provide-card center">
-                                <div className="provide-text">
-                                    <span className="col-gray">Product </span>
-                                    Development
-                                </div>
-                            </div>
-                            <div className="provide-card center">
-                                <div className="provide-text">Consultancy</div>
-                            </div>
-                            <div className="provide-card center">
-                                <div className="provide-text">
-                                    <span className="col-gray">Network </span>
-                                    Setup
-                                </div>
-                            </div>
-                            <div className="provide-card center">
-                                <div className="provide-text">
-                                    <span className="col-gray">DEX </span>
-                                    Creation
-                                </div>
-                            </div>
-                            <div className="provide-card center">
-                                <div className="provide-text">
-                                    <span className="col-gray">Algo </span>
-                                    Trading
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <WeProvide/>
         </div>
     );
 };
