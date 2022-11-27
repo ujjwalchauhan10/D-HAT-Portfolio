e01e6bb0d04e04ed75d73007527bb50d3246801d
ujjwalchauhan10 <ujjwalchauhan247@gmail.com>
2022-11-27 19:57:19 +0530

    Completing we built

--- src/Components/WeBuilt.jsx
+++ src/Components/WeBuilt.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import built from "../Assets/built.svg"
 const WeBuilt = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <div>
+            <div className="container-fluid bg-4">
+                <div className="row">
+                    <div className="what">
+                        What
+                        <span className="we-have px-1">we have built</span>
+                    </div>
+                </div>
+                <div className="row center">
+                    <div className="col-10 center networks-img">
+                        <div>
+                            <img src={built} alt="" className="img-fluid" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default WeBuilt
+export default WeBuilt;
