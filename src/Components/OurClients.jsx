f5f36be2d0cee899a431b9d37830625b227146ee
ujjwalchauhan10 <ujjwalchauhan247@gmail.com>
2022-11-27 20:42:02 +0530

    Completing Clients

--- src/Components/OurClients.jsx
+++ src/Components/OurClients.jsx
@@ -1,11 +1,134 @@
-import React from 'react'
+import React from "react";
+import Acc from "../Assets/Client/Acc.svg";
+import Battle from "../Assets/Client/Battle.svg";
+import Bobble from "../Assets/Client/Bobble.svg";
+import Cryptic from "../Assets/Client/Cryptic.svg";
 
 const OurClients = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <div>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="our-clients">Our Clients</div>
+                </div>
+                <div className="row center client-spacer">
+                    <div className="col-10">
+                        <div className="marquee">
+                            <div className="marquee__group">
+                                <a
+                                    href="https://www.bobble.ai/en/home"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={Bobble}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.battleastra.com/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={Battle}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="http://www.accxchange.in/einvoicing/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={Acc}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://crypticintel.com/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={Cryptic}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                            </div>
 
-export default OurClients
+                            <div aria-hidden="true" className="marquee__group">
+                                <a
+                                    href="https://www.bobble.ai/en/home"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={Bobble}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://www.battleastra.com/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={Battle}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="http://www.accxchange.in/einvoicing/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={Acc}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                                <a
+                                    href="https://crypticintel.com/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <div>
+                                        <img
+                                            src={Cryptic}
+                                            alt=""
+                                            className="iconImages"
+                                        />
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default OurClients;
